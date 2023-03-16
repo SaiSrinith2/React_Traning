a8978c8086c87f8fb9e303270dbd488e1f27ff9f
saisrinith <saisrinith.kondapaka@synergy-ai.us>
2023-03-16 10:10:43 +0530

    adding react ready

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App()
     <div className='mt-5 container'>
       <div className='card'>
         <div className='card-header'>
-          My Action Plan
+          My Action Plan Ready
         </div>
         <div className='card-body'>
           <TodoTable todos={todos} deleteTodo={deleteTodo}/>
